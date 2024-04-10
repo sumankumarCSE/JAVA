0ca0d62b0f340f1f6433511b949ea007ddf93d98
sumankumarCSE <sumankumar843424@gmail.com>
2024-04-11 01:57:23 +0530

    Add some

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,15 @@ package JAVA;
 public class HelloWorld {
     public static void main(String[] args) {
       System.out.println("Hello World");
+      System.out.print("HEllo suman!\n");
+        System.out.println("HEllo suman!");
+        System.out.print("HEllo suman!");
+        int a=10;
+        int b=10;
+        int sum=a+b;
+        System.out.println(sum);
     }
   }
   
+
+        
