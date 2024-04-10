0e808bff830f95b2fd13d2ae191f2cea52a154e9
sumankumarCSE <sumankumar843424@gmail.com>
2024-04-11 02:05:20 +0530

    New

---
+++ Take_input.java
@@ -0,0 +1,17 @@
+package JAVA;
+import java.util.*;
+public class Take_input {
+    public static void main(String args[]) {
+    Scanner sc = new Scanner(System.in);
+    String name1 = sc.next();
+    String name2 = sc.nextLine();
+    System.out.println(name2);
+    System.out.println(name1);
+    System.out.println("sum");
+    int a = sc.nextInt();
+    float b = sc.nextFloat();
+    float sum = a+b;
+    System.out.println(sum); 
+   }
+}
+
