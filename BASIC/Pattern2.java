f69f9846d3482a201794affa66a8c42cfcf46ffd
sumankumarCSE <sumankumar843424@gmail.com>
2024-04-12 21:38:46 +0530

    Add new

--- BASIC/Pattern2.java
+++ BASIC/Pattern2.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Pattern2 {
     public static void main(String args[]) {
     Scanner sc = new Scanner(System.in);
-    System.out.print("Enter the value of n:");
+    System.out.print("Type the value of n:");
     int n = sc.nextInt();
     int i,j;
 
