ef96baaa27b57e4deaa39e124d255cadb928e197
sumankumarCSE <sumankumar843424@gmail.com>
2024-04-14 23:58:08 +0530

    Add new

--- BASIC/Function.java
+++ BASIC/Function.java
@@ -1,10 +1,10 @@
 package JAVA.BASIC;
 import java.util.*;
-public class Function {
-    // public static int calculateSum(int a , int b){
-    //     int sum = a+b;
-    //     return sum;
-    // }
+public class Function{
+    public static int calculateSum(int a , int b){
+        int sum = a+b;
+        return sum;
+    }
     public static void printFactorial(int n){
         if(n<0){System.out.print("invalid"); return;}
         else if(n==0){System.out.println("factorial=1");return;}
@@ -19,11 +19,11 @@ public class Function {
         Scanner sc =new Scanner(System.in) ;
         System.out.print("Enter number:");
         int n=sc.nextInt();     
-        // int a=sc.nextInt();     
+        // int a = sc.nextInt();     
         // int b=sc.nextInt();
         // int sum=calculateSum(a,b);
         // System.out.println("Sum of 2 numbers is:"+ sum);     
-        // System.out.println("Sum of 2 numbers is:"+ calculateSum(a,b)) ;     
+        // System.out.println("Sum of 2 numbers is:"+ calculateSum(a,b));     
         printFactorial(n);
       }
     }
