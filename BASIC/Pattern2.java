2da46720b69d655bb277ffed46964623d3d455bc
sumankumarCSE <sumankumar843424@gmail.com>
2024-04-15 21:24:28 +0530

    Add new

--- BASIC/Pattern2.java
+++ BASIC/Pattern2.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Pattern2{
     public static void main(String args[]) {
     Scanner sc = new Scanner(System.in);
-    System.out.print("Enter the value of n from comu:");
+    System.out.print("Enter the value of n from comut:");
     int n = sc.nextInt();
     int i,j;
 
