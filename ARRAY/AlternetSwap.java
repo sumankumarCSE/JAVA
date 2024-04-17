a5175a30c8f2ccea60f57ebcaaf812796adb7724
sumankumarCSE <sumankumar843424@gmail.com>
2024-04-17 10:42:16 +0530

    Add new

---
+++ ARRAY/AlternetSwap.java
@@ -0,0 +1,38 @@
+package JAVA.ARRAY;
+import java.util.Scanner;
+public class AlternetSwap {
+    public static void swapA(int arr[],int n){
+        for(int i=0;i<n;i+=2){
+         if(i+1<n){
+            int temp=arr[i];
+            arr[i]=arr[i+1];
+            arr[i+1]=temp;
+            }
+        }
+    }
+    public static void printA(int arr[],int n){
+        for(int i=0;i<n;i++){
+            System.out.print(arr[i]+" ");
+        }   
+    }
+    public static void main(String[] args){
+    Scanner sc = new Scanner(System.in); 
+    System.out.print("Enter the size of array:");
+    int n=sc.nextInt(); 
+    int[] arr = new int[n];
+    System.out.println("Enter array Element:");
+    for(int i=0;i<n;i++){
+        arr[i]=sc.nextInt();
+    } 
+    // for(int i=0;i<n;i++){
+    //     if(i%2==0){
+    //         int temp=arr[i];
+    //         arr[i]=arr[i+1];
+    //         arr[i+1]=temp;
+    //     }
+    // }
+    //printA(arr,n);
+    swapA(arr,n);
+    printA(arr,n);
+    }
+}
---
+++ ARRAY/Unique.java
@@ -0,0 +1,5 @@
+package JAVA.ARRAY;
+
+public class Unique {
+    
+}
