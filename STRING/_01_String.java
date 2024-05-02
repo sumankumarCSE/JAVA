b201248fc65a3c5b9fca4463aa7d71764847482c
sumankumarCSE <sumankumar843424@gmail.com>
2024-05-02 22:08:38 +0530

    Add new

--- STRING/_01_String.java
+++ STRING/_01_String.java
@@ -27,7 +27,7 @@ public class _01_String {
      // compare
 
      String name1 = "sumankumar";
-     String name2 = "suman";
+     String name2 = "sumank";
 
     //   s1 > s2  : +ve value
     //   s1 = s2  : 0
