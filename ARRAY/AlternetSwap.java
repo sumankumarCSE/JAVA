9c8edd9646e4ef0d857693942e6875c371306910
sumankumarCSE <sumankumar843424@gmail.com>
2024-05-06 20:38:12 +0530

    Add new

--- ARRAY/AlternetSwap.java
+++ ARRAY/AlternetSwap.java
@@ -43,7 +43,7 @@ public class AlternetSwap {
     //         arr[i+1]=temp;
     //     }
     // }
-    System.out.println("");
+    System.out.println("First");
     //printA(arr,n);
     System.out.println();
     swapA(arr,n);
