fabbaaf5b4d26e727a5c908cecd356c7cf1e28c6
sumankumarCSE <sumankumar843424@gmail.com>
2024-05-12 21:08:06 +0530

    Add new

--- ARRAY/BinarySearch.java
+++ ARRAY/BinarySearch.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class BinarySearch {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter size of the Array: ");
+        System.out.print("Enter size of the Array element: ");
         int n = sc.nextInt();
         int [] arr = new int[n];
         System.out.print("Enter " + n + " Elements: "); 
