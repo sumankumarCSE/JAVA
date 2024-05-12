a9c75355b076254be0a16bfa2bad7d8d891b5bd0
sumankumarCSE <sumankumar843424@gmail.com>
2024-05-12 21:08:27 +0530

    Add new

--- ARRAY/BinarySearch.java
+++ ARRAY/BinarySearch.java
@@ -7,7 +7,7 @@ public class BinarySearch {
         System.out.print("Enter size of the Array element: ");
         int n = sc.nextInt();
         int [] arr = new int[n];
-        System.out.print("Enter " + n + " Elements: "); 
+        System.out.print("Enter " + n + " Elements in Array: "); 
         for(int i=0; i<n; i++)
             arr[i] = sc.nextInt();
         System.out.println(Arrays.toString(arr));
