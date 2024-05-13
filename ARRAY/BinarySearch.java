268e782cf37c0b94cbaab55659b17187dc8d66f0
sumankumarCSE <sumankumar843424@gmail.com>
2024-05-13 21:14:55 +0530

    Add new

--- ARRAY/BinarySearch.java
+++ ARRAY/BinarySearch.java
@@ -16,7 +16,7 @@ public class BinarySearch {
         int f=FirstOccu(arr, target);
         int l=LastOccu(arr, target);
         System.out.println(target + " Found at Index: " + search(arr, target)); //
-        System.out.println("");
+        System.out.println("h");
         System.out.println(target + " First Accurance at Index: " + f);
         System.out.println(target + " Last Accurance at Index: " + l);
         int t=((l-f)+1);
